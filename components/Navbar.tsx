28d47ba9878c470c48b9d62bcf27d103137972f7
tmserl <tom.earl@hey.com>
2022-02-22 16:11:19 +0000

    Started work on beach background when in desktop size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,20 @@
 import Image from 'next/image';
 import styles from './styles/Navbar.module.css';
 import logo from '../public/beyonderbound.png';
+import beachImg from '../public/beach.png';
 
 function Navbar() {
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <Image src={logo} alt="Beyonderbound" placeholder="blur" />
+    <header>
+      <div className={styles.beach}>
+        <Image src={beachImg} className={styles.clip} />
       </div>
-      <nav className={styles.nav}>Nav</nav>
+      <div className={styles.flex}>
+        <div className={styles.logo}>
+          <Image src={logo} alt="Beyonderbound" placeholder="blur" />
+        </div>
+        <nav className={styles.nav}>Nav</nav>
+      </div>
     </header>
   );
 }
