d9d3f31857b7d2ea14e3504d34540fe349fb6516
tmserl <tom.earl@hey.com>
2022-02-23 11:17:44 +0000

    Added component comment at the top and removed usage from homepage

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,3 +1,13 @@
+{
+  /*
+  * Generic Heading Component *
+  -- For use when a generic H2 is needed.
+  -- Props:
+    -- text: The text to appear in the heading
+    -- justify: Choice of 'left', 'center', 'right'
+  */
+}
+
 import React from 'react';
 import styles from './styles/Heading.module.css';
 
