fe4ab01c7dc0f4e9743370d85ae4d6d9d2e60479
“ilhamabdi” <ilhamabdi13@gmail.com>
2022-02-23 16:32:21 +0000

    Glass Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,13 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Glasscard from '../components/Glasscard'
+import Glasssection from '../components/Glasssection';
 
 function Home<NextPage>() {
   return (
     <>
       <p>123</p>
-      <Glasscard text="Top 2022 Destinations" imageurl="https://images.unsplash.com/photo-1473116763249-2faaef81ccda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1496&q=80"/>
-    < Glasscard text= "Visit Paris" imageurl= "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420&q=80"/>
-    < Glasscard text= "Visit Paris" imageurl= "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420&q=80"/>
+      <Glasssection/>
     </>
   );
 }
