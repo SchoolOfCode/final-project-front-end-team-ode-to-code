90c0802631b6c850420405711f1be9d44b22dee2
Najma Elmi <nelmi199@gmail.com>
2022-02-24 21:38:59 +0000

    made a start on hamburger functionality - side navbar currently renders when hamburger icon is clicked

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image';
 import styles from './styles/Navbar.module.css';
+import React, { useState } from 'react';
+import RightNav from './RightNav';
 
 const logo =
   'https://ik.imagekit.io/wletmmv9huf/beyonderbound_6sCbv4RPO.png?ik-sdk-version=javascript-1.4.3&updatedAt=1645615669388';
 
 function Navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(true);
+
   return (
     <header className={styles.header}>
       <div className={styles.headerNav}>
@@ -12,7 +16,12 @@ function Navbar() {
           <Image src={logo} alt="Beyonderbound" height={76} width={479} />
         </div>
         <div className={styles.hamburgerContainer}>
-          <nav className={styles.hamburger}></nav>
+          <nav
+            onClick={() => setNavbarOpen(!navbarOpen)}
+            className={styles.hamburger}
+          ></nav>
+          {/*  {navbarOpen && <RightNav />} */}
+          {!navbarOpen ? <RightNav /> : <></>}
         </div>
       </div>
     </header>
