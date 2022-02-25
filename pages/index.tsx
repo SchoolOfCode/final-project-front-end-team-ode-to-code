20132f23933aca9e2b612f5cd5012afacc0b5526
RedGeorge <94233923+RedGeorge@users.noreply.github.com>
2022-02-25 13:19:57 +0000

    Added the basic carousel feature to the home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css';
 import WaveImage from '../components/WaveImage';
 import Glasssection from '../components/Glasssection';
 import SearchSection from '../components/SearchSection';
+import Carousel from '../components/Carousel';
 
 function Home<NextPage>() {
   return (
@@ -12,6 +13,7 @@ function Home<NextPage>() {
       <WaveImage />
       <SearchSection />
       <Glasssection />
+      <Carousel />
     </>
   );
 }
