d9f049399654f6862065719bfa80dddcfdf0d77d
HannahWhelan256 <hannahwhelan256@gmail.com>
2022-02-25 14:21:02 +0000

    updated images

--- components/Glasssection.tsx
+++ components/Glasssection.tsx
@@ -1,4 +1,5 @@
 import GlassCard from './Glasscard';
+import { images } from '../lib/images'
 
 import styles from './styles/Glasssection.module.css';
 
@@ -7,15 +8,15 @@ export default function Glasscection() {
     <section className={styles.section}>
       <GlassCard
         text="Top 2022 Destinations"
-        imageUrl="https://images.unsplash.com/photo-1473116763249-2faaef81ccda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1496&q=80"
+        imageUrl={images.article1}
       />
       <GlassCard
         text="Travel Tips"
-        imageUrl="https://images.unsplash.com/photo-1473116763249-2faaef81ccda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1496&q=80"
+        imageUrl={images.article2}
       />
       <GlassCard
         text="Where's hot now?"
-        imageUrl="https://images.unsplash.com/photo-1473116763249-2faaef81ccda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1496&q=80"
+        imageUrl={images.article3}
       />
     </section>
   );
