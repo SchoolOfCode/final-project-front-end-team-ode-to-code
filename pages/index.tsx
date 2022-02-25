463df006ffbaeb6bd364abf4d056d2a3062b99a4
tmserl <tom.earl@hey.com>
2022-02-25 10:56:38 +0000

    Testing mapping through city_names

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,33 @@ import WaveImage from '../components/WaveImage';
 import Glasssection from '../components/Glasssection';
 import SearchSection from '../components/SearchSection';
 
-function Home<NextPage>() {
+const citiesApi = 'https://four-week-project.herokuapp.com/cities';
+const countriesApi = 'https://four-week-project.herokuapp.com/countries';
+
+export async function getStaticProps() {
+  const resCities = await fetch(citiesApi);
+  const resCountries = await fetch(countriesApi);
+  const dataCities = await resCities.json();
+  const dataCountries = await resCountries.json();
+
+  return {
+    props: { cities: dataCities.payload, countries: dataCountries.payload },
+  };
+}
+
+function Home<NextPage>({ cities, countries }) {
   return (
     <>
       <WaveImage />
       <SearchSection />
       <Glasssection />
+      <div>
+        {cities.map((city) => (
+          <div key={city.id}>
+            <h2>{city.city_name}</h2>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
