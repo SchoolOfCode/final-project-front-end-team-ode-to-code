2c871073f213875515257ebd6b8533514057940f
HannahWhelan256 <hannahwhelan256@gmail.com>
2022-02-28 00:58:32 +0000

    conditional rendering of header image, example carousel image

--- pages/feelinghot.tsx
+++ pages/feelinghot.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next';
 import WaveImage from '../components/WaveImage';
 import {images} from '../lib/images'
+import styles from '../components/styles/Article.module.css';
 
 function FeelingHot<NextPage>() {
   return (
     <>
       <WaveImage imageUrl={images.article3L} />
+      <h1 className={styles.heading}>Where's Hot Now?</h1>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,15 @@ import SearchSection from '../components/SearchSection';
 import {images} from '../lib/images'
 import Carousel from '../components/Carousel';
 import Heading from '../components/Heading'
+// import { useMediaQuery } from 'react-responsive';
 
 
 function Home<NextPage>() {
+  // const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
   return (
     <>
-      <WaveImage imageUrl={images.homepage}/>
+      {/* {isMobile? <Image className={styles.logo} src={images.logo} alt="Beyonderbound" height={38} width={239} /> : <></>} */}
+      <WaveImage imageUrl={images.homepage} />
       <SearchSection />
       <div className='wrapper wrapper--lg'><Heading text="Countries to discover..." justify="left"/></div>
       <Carousel />
