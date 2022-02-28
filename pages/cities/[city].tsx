4274aa617667f7ba0c3f2a8882886af0642a0c44
tmserl <tom.earl@hey.com>
2022-02-28 17:33:54 +0000

    Swapped manual h2 with Heading component

--- pages/cities/[city].tsx
+++ pages/cities/[city].tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import PageTitle from '../../components/PageTitle';
+import Heading from '../../components/Heading';
 
 const citiesApi = 'https://four-week-project.herokuapp.com/cities';
 
@@ -87,10 +88,8 @@ function City({ city }: { city: any }) {
             }
           })}
         </p>
-        <br />
-        <h2>All About {city.city_name}</h2>
+        <Heading text={city.city_name} justify="left" />
         <p>{city.city_description}</p>
-        <br />
         {/* set actual country link site */}
         <h2>
           Back to{' '}
