838c358d231c88858b05d729e5a0524d70638c2c
HannahWhelan256 <hannahwhelan256@gmail.com>
2022-03-04 00:10:38 +0000

    removed old code from context

--- context/state.js
+++ context/state.js
@@ -1,9 +1,5 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 
-// const citiesApi = 'https://four-week-project.herokuapp.com/cities';
-// const countriesApi = 'https://four-week-project.herokuapp.com/countries';
-
-
 export const AppContext = createContext();
 
 export function AppWrapper({ children }) {
