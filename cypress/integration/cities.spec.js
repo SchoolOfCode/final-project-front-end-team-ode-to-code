58adb8d5a5c0528226900c0b9b97706cf860d1f0
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-03-08 11:49:59 +0000

    new test api

--- cypress/integration/cities.spec.js
+++ cypress/integration/cities.spec.js
@@ -1,6 +1,8 @@
+
+
 describe("renders a cities page", () => {
     beforeEach(() => {
-      cy.visit("http://localhost:3000/cities/Florence");
+      cy.visit("http://localhost:3000/cities/Paris");
     });
 
 ///should be clickable
@@ -8,7 +10,24 @@ describe("renders a cities page", () => {
         cy.get('[data-cy=city-back-button]').click();
         });
 ///css check
-        it.skip("should have css margin", () => {
+        it("should have css margin", () => {
             cy.get('[data-cy=city-description]').should("have.css","margin")
             });
+            
+/// fetch cities API status
+it.skip("should fetch the cities infos", () => {
+// wrap null, so that you can use `then` to invoke a callback as an arbitrary command
+// and return a Cypress.Promise - so that cypress will wait on it's result
+// (or just use cy.request :))
+      try {
+        cy.request("GET","http://localhost:3000/cities/Paris").then((req ,res) => {
+        expect(req.status).to.eq(200)
+        expect(req).to.be.an('object')
+        expect(req).to.have.property('headers')
+        expect(req.isOkStatusCode).to.eq(true)   
+})
+      } catch (e) {
+        console.log(e,'Error')
+      }
+  })
 });
--- cypress/integration/countries.spec.js
+++ cypress/integration/countries.spec.js
@@ -8,4 +8,18 @@ describe("renders a cities page", () => {
         });
 
       
+        /// fetch cities API status
+it("should fetch the countries infos", () => {
+  // wrap null, so that you can use `then` to invoke a callback as an arbitrary command
+  // and return a Cypress.Promise - so that cypress will wait on it's result
+  // (or just use cy.request :))
+        try {
+          cy.request("http://localhost:3000/countries/France").then((req ,res) => {
+          expect(req.status).to.eq(200)
+          expect(req.isOkStatusCode).to.eq(true)
+  })
+        } catch (e) {
+          console.log(e,'Error')
+        }
+    })
 });
