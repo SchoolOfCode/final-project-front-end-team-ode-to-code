ccca078d95c6e9a97322cf4e123e7a10b06d2294
RedGeorge <94233923+RedGeorge@users.noreply.github.com>
2022-03-08 14:40:08 +0000

    Began working on advanced search dropdown layout and buttons.

--- components/SearchSection.tsx
+++ components/SearchSection.tsx
@@ -27,6 +27,34 @@ export default function SearchSection({handleChange,handleSubmit,luckyDip}:any) 
       <div className={styles.buttons}>
         {/* <p>￡:pound,👨‍👩‍👧‍👦:family,🚶‍♀️:solo,💆:city break,</p>
         <p>👫:romance,👢Shopping,🌞:sun,🌝:honeymoon,💃🕺🏻:night life</p> */}
+        
+        <div className="dropdown">
+          <button className={styles.dropdownButton}>Advanced</button>
+            <div className="advanced-dropdown-content">
+              <div className="holiday-type-dropdown">
+                <button className={styles.buttons}>Holiday Type</button>
+                  <div className="holiday-type-dropdown-content">
+                    <button className={styles.buttons}>Sun Holiday</button>
+                    <button className={styles.buttons}>City Break</button>
+                    <button className={styles.buttons}>Adventure</button>
+                    <button className={styles.buttons}>Wintry Holiday</button>
+                  </div>
+              </div>
+    <a href="#">Budget</a>
+    <a href="#">Rating</a>
+    <div className="continent-dropdown">
+                <Button text="Continent"/>
+                  <div className="continent-dropdown-content">
+                    <Button text="Europe"/>
+                    <Button text="North America"/>
+                    <Button text="South America"/>
+                    <Button text="Africa"/>
+                    <Button text="Australia"/>
+                    <Button text="Asia"/>
+                  </div>
+              </div>
+  </div>
+</div>
        <Button text="Lucky Dip" luckyDip={luckyDip}/>
       </div>
     </div>
