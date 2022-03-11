107769fd1490101ea259122ff1394b6e10fd7ef3
HannahWhelan256 <hannahwhelan256@gmail.com>
2022-03-11 00:33:40 +0000

    removed unused components, created flip cards for About page

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,10 +3,10 @@ import PageTitle from '../components/PageTitle';
 import { images } from '../lib/images';
 import styles from '../styles/About.module.css';
 import Link from 'next/link';
-import TeamMember from '../components/TeamMember';
 import { teamOdeToCode } from '../lib/team';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Card from '../components/Card';
 
 function About<NextPage>() {
   return (
@@ -27,15 +27,15 @@ function About<NextPage>() {
           <p className={styles.container}>
             We met via{' '}
             <Link href="https://schoolofcode.co.uk/">School of Code</Link>'s
-            first national bootcamp, and were grouped together for our Final 4
+            first national bootcamp, and were grouped together for our final 4
             week project. Our challenge was to work together remotely, using
             Agile Methodologies, to build a full stack application. Thus,{' '}
             <span className={styles.highlight}>beyonderbound</span> was born!
           </p>
           <p className={styles.container}>
-            For more information feel free to checkout our pages:
+            For more information feel free to check out our pages:
           </p>
-          <ul className={styles.list}>
+          {/* <ul className={styles.list}>
             {teamOdeToCode.map((teamMember) => (
               <li>
                 <TeamMember
@@ -47,7 +47,19 @@ function About<NextPage>() {
                 />
               </li>
             ))}
-          </ul>
+          </ul> */}
+          <div className={styles.profilecards}>
+          {teamOdeToCode.map((teamMember) => (
+            <Card
+              key={teamMember.Name}
+              name={teamMember.Name}
+              image={teamMember.Image}
+              linkedin={teamMember.Linkedin}
+              github={teamMember.GitHub}
+              portfolio={teamMember.Portfolio}
+            />
+          ))}
+        </div>
         </div>
       </Layout>
     </>
