e62157dc91adcc3921e1c3d99149ce38397ea0a5
HannahWhelan256 <hannahwhelan256@gmail.com>
2022-03-15 14:27:42 +0000

    added GET functionality

---
+++ components/CountryTile.tsx
@@ -0,0 +1,15 @@
+import styles from './styles/CityTile.module.css';
+
+function CountryTile({ country }: { country: any }) {
+    
+  return (
+    <div className={styles.container}>
+    <p><span className={styles.header}>Country: {country.country}</span><br/>
+    <span className={styles.strong}>Continent: </span>{country.continent}<br/>
+    <span className={styles.strong}>Image: </span><a href={country.image}>{country.image}</a><br/>
+    <span className={styles.strong}>Image2: </span><a href={country.image2}>{country.image2}</a><br/>
+    <span className={styles.strong}>Country Description: </span>{country.country_description}<br/>
+    <span className={styles.strong}>Cities: </span>{country.cities.join(', ')}</p></div>)
+}
+
+export default CountryTile;
--- pages/admin.jsx
+++ pages/admin.jsx
@@ -3,23 +3,84 @@ import PageTitle from '../components/PageTitle';
 import { images } from '../lib/images';
 import styles from '../styles/Admin.module.css';
 import Head from 'next/head';
+import { useState, useContext } from 'react';
+import Button from '../components/Button';
+import { AppContext } from '../context/state';
+import CityTile from '../components/CityTile';
+import CountryTile from '../components/CountryTile';
+import CityCountryDropDown from '../components/CityCountryDropDown';
 
 function Admin() {
-    const citiesApi = 'https://four-week-project.herokuapp.com/cities';
-    const countriesApi = 'https://four-week-project.herokuapp.com/countries';
+  const { data, isLoading } = useContext(AppContext);
+  const citiesApi = 'https://four-week-project.herokuapp.com/cities';
+  const countriesApi = 'https://four-week-project.herokuapp.com/countries';
+  const [action, setAction] = useState('');
+  const [cityOrCountry, setCityOrCountry] = useState('');
 
-    
+  function selectedAction(e) {
+    const value = e.target.value;
+    setAction(value);
+    setCityOrCountry("");
+  }
 
-    return (
-        <>
-          <Head>
-            <title>beyonderbound | Admin</title>
-          </Head>
-          <Layout imageUrl={images.admin}>
-            <PageTitle text="Admin" />
-          </Layout>
-        </>
-    )
+  function selectCityorCountry(e) {
+    const value = e.target.value;
+    setCityOrCountry(value);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>beyonderbound | Admin</title>
+      </Head>
+      <Layout imageUrl={images.admin}>
+        <PageTitle text="Admin" />
+        <div className={styles.body}>
+          <form className={styles.dropdown}>
+            <label for="actionType">Select action: </label>
+            <select onChange={selectedAction} name="actionType">
+              <option value="">...</option>
+              <option value="get">GET all cities/countries</option>
+              <option value="post">POST a new city/country</option>
+              <option value="put">REPLACE a city/country</option>
+              <option value="patch">EDIT a city/country</option>
+              <option value="delete">DELETE a city/country</option>
+            </select>
+          </form>
+          {action === 'get' && (
+            <>
+              <CityCountryDropDown action={selectCityorCountry}/>
+              {/* if city selected, display tiles with all city data, sorted alphabetically */}
+              {cityOrCountry === 'city' &&
+                data.cities
+                  .sort(function (a, b) {
+                    return a.city_name < b.city_name
+                      ? -1
+                      : a.city_name > b.city_name
+                      ? 1
+                      : 0;
+                  })
+                  .map((city) => <CityTile key={city} city={city} />)}
+              {/* if country selected, display tiles with all country data, sorted alphabetically */}
+              {cityOrCountry === 'country' &&
+                data.countries
+                  .sort(function (a, b) {
+                    return a.country < b.country
+                      ? -1
+                      : a.country > b.country
+                      ? 1
+                      : 0;
+                  })
+                  .map((country) => (
+                    <CountryTile key={country} country={country} />
+                  ))}
+            </>
+          )}
+          {action === "post" && (<CityCountryDropDown action={selectCityorCountry}/>)}
+        </div>
+      </Layout>
+    </>
+  );
 }
 
 export default Admin;
